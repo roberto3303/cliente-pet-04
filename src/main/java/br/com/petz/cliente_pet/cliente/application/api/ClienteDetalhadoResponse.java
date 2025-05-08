d70e26933e749e9725177b31799a580db007fd79
Roberto Pedro <robertopedro100@gmail.com>
2025-05-08 19:42:44 -0300

    Implementa Busca por id(sem handler), teste ok!

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
 import br.com.petz.cliente_pet.cliente.domain.Sexo;
 import lombok.Value;
 
@@ -18,4 +19,17 @@ public class ClienteDetalhadoResponse {
     private Boolean aceitaTermos;
 
     private LocalDateTime dataHoraDoCadastro;
+
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.telefone = cliente.getTelefone();
+        this.sexo = cliente.getSexo();
+        this.aceitaTermos = cliente.getAceitaTermos();
+        this.dataHoraDoCadastro = cliente.getDataHoraDoCadastro();
+    }
+
 }
--- src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -28,5 +29,14 @@ public class ClienteInfraRepository implements ClienteRepository {
         List<Cliente> todosClientes = clienteSpringDataJPARepository.findAll();
         log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
         return todosClientes;
+    }
+
+    @Override
+    public Cliente buscaClienteAtravesId(UUID idCliente) {
+        log.info("[inicia] ClienteInfraRepository - buscaClienteAtravesId");
+        Cliente cliente = clienteSpringDataJPARepository.findById(idCliente)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado!"));
+        log.info("[finaliza] ClienteInfraRepository - buscaClienteAtravesId");
+        return cliente;
     }
 }

85f36733e0a4c21436b8fb389b96138e2d26616a
Roberto Pedro <robertopedro100@gmail.com>
2025-05-28 17:01:37 -0300

    Implementa camada api fluxo alteracao

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -46,4 +46,11 @@ public class ClienteController implements ClienteAPI {
         clienteService.deletaClienteAtravesId(idCliente);
         log.info("[finaliza] ClienteController - deletaClienteAtravesId");
     }
+
+    @Override
+    public void patchAlteraCliente(UUID idCliente, ClienteAlteracaoRequest clienteAlteracaoRequest) {
+        log.info("[inicia] ClienteController - patchAlteraCliente");
+        log.info("[idCliente] {}", idCliente);
+        log.info("[finaliza] ClienteController - patchAlteraCliente");
+    }
 }

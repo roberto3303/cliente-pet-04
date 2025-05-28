4510a5e7d5539a5e2b5aea8b9b3334c6345430db
Roberto Pedro <robertopedro100@gmail.com>
2025-05-28 15:44:02 -0300

    Implementa tratamento de erro para excecao duplicada

--- src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
@@ -2,8 +2,11 @@ package br.com.petz.cliente_pet.cliente.infra;
 
 import br.com.petz.cliente_pet.cliente.application.repository.ClienteRepository;
 import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import br.com.petz.cliente_pet.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,7 +21,11 @@ public class ClienteInfraRepository implements ClienteRepository {
     @Override
     public Cliente salva(Cliente cliente) {
         log.info("[inicia] ClienteInfraRepository - salva");
-        clienteSpringDataJPARepository.save(cliente);
+        try {
+            clienteSpringDataJPARepository.save(cliente);
+        } catch (DataIntegrityViolationException e) {
+            throw APIException.build(HttpStatus.BAD_REQUEST,"Existem dados duplicados!");
+        }
         log.info("[finaliza] ClienteInfraRepository - salva");
         return cliente;
     }

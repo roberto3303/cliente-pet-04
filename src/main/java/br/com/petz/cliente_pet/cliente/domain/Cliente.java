c3a47be3f3580c5b370cf490987babfc3cb09a30
Roberto Pedro <robertopedro100@gmail.com>
2025-05-28 17:31:43 -0300

    Alteracao finalizado, teste ok!

--- src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.cliente.domain;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteAlteracaoRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -53,5 +54,15 @@ public class Cliente {
         this.cpf = clienteRequest.getCpf();
         this.aceitaTermos = clienteRequest.getAceitaTermos();
         this.dataHoraDoCadastro = LocalDateTime.now();
+    }
+
+    public void altera(ClienteAlteracaoRequest clienteAlteracaoRequestRequest) {
+        this.nomeCompleto = clienteAlteracaoRequestRequest.getNomeCompleto();
+        this.celular = clienteAlteracaoRequestRequest.getCelular();
+        this.telefone = clienteAlteracaoRequestRequest.getTelefone();
+        this.sexo = clienteAlteracaoRequestRequest.getSexo();
+        this.dataNascimento = clienteAlteracaoRequestRequest.getDataNascimento();
+        this.aceitaTermos = clienteAlteracaoRequestRequest.getAceitaTermos();
+        this.dataHoraDaUltimaAlteracao = LocalDateTime.now();
     }
 }
